3041cd716aa9ff3d8695d1c24f20104a877e6189
DinkeyYT <46655259+DinkeyYT@users.noreply.github.com>
2021-05-16 22:39:17 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+settings = {
+    'token' : 'ODQzNDc0MjU2ODU5ODg5NjY1.YKEYnQ.XscFA8A6wr63Sp9tnX1yahEK4-Q',
+    'bot' : 'ＳＫＹ ＡＮＧＥＬ',
+    'id' : 843474256859889665,
+    'prefix': ''
+
+
+
+}
---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+import logging
+from discord.ext import commands
+from config import settings
+bot = commands.Bot(command_prefix='>')
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+    bot.run('ODQzNDc0MjU2ODU5ODg5NjY1.YKEYnQ.XscFA8A6wr63Sp9tnX1yahEK4-Q')
+
+    class MyClient(discord.Client):
+        async def on_ready(self):
+            print('Logged on as', self.user)
+
+        async def on_message(self, message):
+            # don't respond to ourselves
+            if message.author == self.user:
+                return
+
+            if message.content == 'ping':
+                await message.channel.send('pong')
+
+    client = MyClient()
+    client.run('ODQzNDc0MjU2ODU5ODg5NjY1.YKEYnQ.XscFA8A6wr63Sp9tnX1yahEK4-Q')
